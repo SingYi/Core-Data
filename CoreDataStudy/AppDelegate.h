01ed43d312e441575cb01db379e3b2e1edc86df0
SingYi <23323231@qq.com>
2017-03-14 17:47:07 +0800

    1.0

---
+++ CoreDataStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataStudy
+//
+//  Created by 石燚 on 17/3/14.
+//  Copyright © 2017年 SingYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreDataStudy/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CoreDataStudy
+//
+//  Created by 石燚 on 17/3/14.
+//  Copyright © 2017年 SingYi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
